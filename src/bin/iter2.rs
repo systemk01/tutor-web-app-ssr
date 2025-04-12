a9d682e492a4ab5838be108aa00f03590dbf7d04
systemk01 <systemk1@icloud.com>
2025-04-13 01:23:18 +0200

    writing unit test and integration test for iter2 handlerfunction handle_post_tutor

--- src/bin/iter2.rs
+++ src/bin/iter2.rs
@@ -1,7 +1,8 @@
-use actix_web::{App, Error, HttpResponse, HttpServer, Result, error, web};
+use actix_web::{web, error, App, Error, HttpResponse, HttpServer, Result};
 use serde::{Deserialize, Serialize};
 use std::env;
 use tera::Tera;
+
 
 //store tera template in application state
 async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
@@ -52,4 +53,52 @@ fn app_config(config: &mut web::ServiceConfig) {
             .service(web::resource("/").route(web::get().to(index)))
             .service(web::resource("/tutors").route(web::post().to(handle_post_tutor))),
     );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http::{header::CONTENT_TYPE, header::HeaderValue, StatusCode};
+    use actix_web::web::Form;
+    use actix_web::test::{self};
+    use actix_web::dev::Service;
+
+#[actix_rt::test]
+async fn handle_post_tutor_unit_test() {
+    let params = Form(Tutor{
+        name: "Tooby".to_string(),
+    });
+    let tera = Tera::new(concat!(
+        env!("CARGO_MANIFEST_DIR"), 
+        "/static/iter2/**/*")).unwrap();
+    let webdata_tera = web::Data::new(tera);
+    let resp = handle_post_tutor(webdata_tera, params).await.unwrap();
+    assert_eq!(resp.status(),StatusCode::OK);
+    assert_eq!(
+        resp.headers().get(CONTENT_TYPE).unwrap(),
+        HeaderValue::from_static("text/html")
+    );
+}
+
+//Integration test case
+#[actix_rt::test]
+async fn handle_post_tutor_integration_test() {
+    let tera = Tera::new(concat!(
+        env!("CARGO_MANIFEST_DIR"), 
+        "/static/iter2/**/*")).unwrap();
+        let app = test::init_service(App::new().app_data(web::Data::new(tera)).configure(app_config)).await;
+        let req = test::TestRequest::post()
+        .uri("/tutors")
+        .set_form(&Tutor {
+            name: "Terry".to_string(),
+        })
+        .to_request();
+    let resp = app.call(req).await.unwrap();
+    assert_eq!(resp.status(), StatusCode::OK);
+    assert_eq!(
+        resp.headers().get(CONTENT_TYPE).unwrap(),
+        HeaderValue::from_static("text/html")
+    );
+}
+
 }
